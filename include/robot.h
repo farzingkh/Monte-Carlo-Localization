b3a1c0ab2426c7fc94d499f323dbefe56a26c058
Farzin Golkhosh <farzin.golkhosh@gmail.com>
2020-08-06 09:59:18 -0700

    [FEAT] Update robot class

--- include/robot.h
+++ include/robot.h
@@ -18,24 +18,16 @@ public:
     // Set robots state transition noise
     void set_noise(double forward_nois, double turn_noise, double sensor_noise);
     // Sense the environment; return a vecotr of measurements from landmarks
-    vector<double> sense();
+    std::vector<double> sense();
     // Move the robot
     void move(double turn, double forward);
     // Get pose readings
     std::string get_pose();
     // Get sensor readings
     std::string get_sensor_readings();
-    // Get measurement probability
-    double get_measurement_prob(std::vector<double> measurements);
 
 private:
-    // Get Gaussian random
-    double get_gaussian_random_number(double mean, double var);
-    // Get normal distribution probability of x given mean and variance
-    double get_gaussian_probability(double mean, double var, double x);
-    // Random Generators
-    random_device rd__;
-    mt19937 gen__(rd__());
+
     // robot states
     double x__;
     double y__;
