0ca673c2fb86d106a7af7f3ea50609776fe638f3
Farzin Golkhosh <farzin.golkhosh@gmail.com>
2020-08-13 13:16:04 -0700

    [FEAT] Print landmarks added

--- src/world.cpp
+++ src/world.cpp
@@ -1,4 +1,5 @@
 #include "../include/world.h"
+#include <iostream>
 #include <assert.h>
 
 World::World(int size_x, int size_y) : x__(size_x), y__(size_y) {}
@@ -9,6 +10,7 @@ void World::set_landmarks(double x, double y)
     landmarks__.push_back(y);
     // check invariant condition
     assert(landmarks__.size() % 2 == 0);
+    std::cout << "[Landmark] x:" << x << " y:" << y <<std::endl;
 }
 
 std::vector<double> World::get_landmarks()
